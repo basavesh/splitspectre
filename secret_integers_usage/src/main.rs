0c8e0debadbe43fd19855424cbee898dffdf4de1
Basavesh Shivakumar <basavesh.shivakmar@gmail.com>
2021-03-05 11:24:12 +0100

    get input from user

--- secret_integers_usage/src/main.rs
+++ secret_integers_usage/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 use rand::Rng;
 use secret_integers::*;
 
@@ -33,8 +34,18 @@ fn decrypt(cipher: &[u8], sk: &[U8]) -> Vec<u8> {
 fn main() {
     let random_bytes = rand::thread_rng().gen::<[u8; 8]>();
     let secret_key = classify_u8s(&random_bytes);
-    let message = rand::thread_rng().gen::<[u8; 8]>();
+    // let message = rand::thread_rng().gen::<[u8; 8]>();
+    let mut buffer = String::new();
+    let _ = io::stdin().read_line(&mut buffer);
+    let mut message = [0u8; 8];
+    for i in 0..8 {
+        if i < buffer.len() {
+            message[i] = buffer.as_bytes()[i];
+        }
+    }
     let cipher_text = encrypt(&message, &secret_key);
     let text = decrypt(&cipher_text, &secret_key);
     assert!(message == &text[..]);
+    println!("message: {:?}", message);
+    println!("ciphertext: {:?}", cipher_text);
 }
