5633684a9de9800a3c10da1b6407285b7876e86d
Basavesh Shivakumar <basavesh.shivakumar@gmail.com>
2021-05-09 13:55:15 +0200

    wip

--- split_tonic/src/agent_server.rs
+++ split_tonic/src/agent_server.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use tonic::{transport::Server, Request, Response, Status};
 use splitspectre::agent_server::{Agent, AgentServer};
 use splitspectre::*;
@@ -11,6 +12,16 @@ pub mod simple;
 pub mod splitspectre {
     // the string specified here must match the proto package name
     tonic::include_proto!("splitspectre");
+}
+
+/// classify vector of u8s into U8s
+fn classify_u8s(v: &[u8]) -> Vec<U8> {
+    v.iter().map(|x| U8::classify(*x)).collect()
+}
+
+/// declassify vector of U8s into u8s
+fn declassify_u8s(v: &[U8]) -> Vec<u8> {
+    v.iter().map(|x| U8::declassify(*x)).collect()
 }
 
 #[derive(Debug, Default)]
@@ -27,14 +38,34 @@ impl Agent for MyAgent {
     ) -> Result<Response<GetSecretKeyResponse>, Status> { // Return respone - keyid
         println!("Got a GetSecretKey Request");
 
+        // TODO change the logic of this
+        // If the callee function contains secret type in the argument,
+        // get a read lock immidiately and get keyid
+        // TODO need to supply some extra information to find that.
+
+        // Only if the return type is secret.
+        // First, check if the result is already stored in the HashMap
+        let call_result = simple::get_secret_key();
+
         if let Ok(mut write_guard) = self.keys_map.write() {
+            for (k, v) in write_guard.iter() {
+                // To compare, should declassify secrets
+                if declassify_u8s(v) == declassify_u8s(&call_result) {
+                    // time to return the result
+                    // we already have the key for this
+                    let response = GetSecretKeyResponse {
+                        result: *k,
+                    };
+                    return Ok(Response::new(response));
+                }
+            }
+            // time to create a new keyid
             let mut num = self.counter.lock().unwrap();
             *num += 1;
-            write_guard.insert(*num, simple::get_secret_key());
+            write_guard.insert(*num, call_result);
             let response = GetSecretKeyResponse {
                 result: *num,
             };
-
             return Ok(Response::new(response));
         }
 
@@ -47,6 +78,8 @@ impl Agent for MyAgent {
     ) -> Result<Response<EncryptResponse>, Status> {
         println!("Got an encrypt Request");
 
+        // If the callee signature doesn't return a secret type,
+        // I should just take the read lock
         if let Ok(read_guard) = self.keys_map.read() {
             let request = request.into_inner();
 
@@ -59,7 +92,7 @@ impl Agent for MyAgent {
 
                 return Ok(Response::new(response));
             }
-            return Err(tonic::Status::unimplemented("Could not obtain lock"));
+            return Err(tonic::Status::unimplemented("No corresponding secret for the key provided"));
         }
 
         Err(tonic::Status::unimplemented("Could not obtain lock"))
@@ -83,7 +116,7 @@ impl Agent for MyAgent {
 
                 return Ok(Response::new(response));
             }
-            return Err(tonic::Status::unimplemented("Could not obtain lock"));
+            return Err(tonic::Status::unimplemented("No corresponding secret for the key provided"));
         }
 
         Err(tonic::Status::unimplemented("Could not obtain lock"))
